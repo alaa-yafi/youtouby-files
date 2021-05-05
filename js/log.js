7bd921b8200be98f9d3822c341dcec61a883966e
alaa-yafi <alaa.yafi25@outlook.com>
2021-05-05 13:25:37 +0300

    somechanges

---
+++ js/log.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+   
+    $(".remove").click(function () {
+        $(this).parents('.card').hide(); 
+    });
+});
+ // الهدف:حذف الفيديو من سجل المشاهدة عندما يُضغط على إشارة الخطأ الموجودة في الزاوية//
